42dc4674e203af8b84301f1bed7eff7046bb8587
Rynco Maekawa <i@rynco.me>
2024-08-20 17:00:51 +0800

    Fix symlinking

--- src/self_ops.rs
+++ src/self_ops.rs
@@ -2,6 +2,9 @@ use std::path::{Path, PathBuf};
 
 use clap::Parser;
 
+/// The MoonBit toolchain multiplexer.
+///
+/// Symlink this binary with other names to call the corresponding tools.
 #[derive(clap::Parser, Debug)]
 struct Cli {
     #[clap(subcommand)]
@@ -13,6 +16,7 @@ enum Cmd {
     Link(LinkSubcommand),
 }
 
+/// Symlink the current binary to the specified path.
 #[derive(clap::Parser, Debug)]
 struct LinkSubcommand {
     /// The target symlink path
@@ -32,24 +36,37 @@ fn handle_link(_cli: &Cli, cmd: &LinkSubcommand) {
     let self_path = std::env::current_exe().unwrap();
 
     #[cfg(windows)]
-    fn do_symlink(from: &Path, to: &Path) {
-        std::os::windows::fs::symlink_file(from, to).unwrap();
+    fn do_symlink(from: &Path, to: &Path) -> anyhow::Result<()> {
+        std::os::windows::fs::symlink_file(from, to)?;
+        Ok(())
     }
 
     #[cfg(unix)]
-    fn do_symlink(from: &Path, to: &Path) {
-        std::os::unix::fs::symlink(from, to).unwrap();
+    fn do_symlink(from: &Path, to: &Path) -> anyhow::Result<()> {
+        std::os::unix::fs::symlink(from, to)?;
+        Ok(())
     }
 
     #[cfg(not(any(windows, unix)))]
-    fn do_symlink(from: &Path, to: &Path) {
+    fn do_symlink(from: &Path, to: &Path) -> anyhow::Result<()> {
         panic!("Unsupported platform, unable to perform symlink");
     }
 
-    do_symlink(&self_path, &cmd.path);
-    eprintln!(
-        "Symlinked {} to {}",
-        self_path.display(),
-        cmd.path.display()
-    );
+    match do_symlink(&self_path, &cmd.path) {
+        Ok(()) => {
+            println!(
+                "Symlinked {} to {}",
+                self_path.display(),
+                cmd.path.display()
+            );
+        }
+        Err(e) => {
+            eprintln!(
+                "Failed to symlink {} to {}: {}",
+                self_path.display(),
+                cmd.path.display(),
+                e
+            );
+        }
+    }
 }
